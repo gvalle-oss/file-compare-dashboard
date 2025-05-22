732b1ce0e7b85db9134e6673553c7cdfe8fcedfd
Gilbert Valle III <gilbert.valle1986@gmail.com>
2025-05-22 14:30:21 -0600

    Rebuild frontend and include correct JS bundle + updated server config

--- app.py
+++ app.py
@@ -82,6 +82,14 @@ def compare_files():
         elif ext1 in ['txt', 'py', 'xml']:
             diff = compare_text(f1, f2)
             return jsonify({'type': 'text', 'diff': diff})
+        
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
